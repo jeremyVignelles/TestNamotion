6d7043549bdb55d04bbbd62da24f0d25c580bc72
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2020-03-05 19:23:03 +0100

    Repro for https://github.com/RicoSuter/Namotion.Reflection/issues/33

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestNamotion
+{
+    using Namotion.Reflection;
+
+    /// <summary>
+    /// This is a XML documentation
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The Main method
+        /// </summary>
+        public static void Main()
+        {
+            // Does not display anything, don't know why...
+            Console.WriteLine(typeof(Program).GetXmlDocsSummary());
+
+            //Displays the XML doc for the Main method
+            Console.WriteLine(typeof(Program).GetMethod(nameof(Main)).GetXmlDocsSummary());
+
+            // Should display the XML document for the method
+            Console.WriteLine(typeof(XmlDocsExtensions).GetMethod(nameof(XmlDocsExtensions.ToXmlDocsContent)).GetXmlDocsSummary());
+        }
+    }
+}
